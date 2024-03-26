5f158174e418b8452078c155dc2c29862f029a5f
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-03-26 09:02:22 +0600

    scaffolding project

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import {createServer} from "http";
+
+const app = express();
+const server = createServer(app);
+
+app.use(
+    helmet({
+        strictTransportSecurity: true,
+    }),
+);
+app.use(cors({origin: "*"}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+server.listen(3000, () => {
+    console.log("server is listening on port: 3000");
+});
