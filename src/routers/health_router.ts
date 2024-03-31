2b4a73e6f07a7ef002e4aa5e4ace0108452a84b1
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-03-31 08:21:56 +0600

    ref: docker compose config update
    let docker use the local host network so that the container can directly
    connect to the cassandra deployment.

---
+++ src/routers/health_router.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import HttpResponse from "../controllers/http_response";
+
+export const healthRouter = Router();
+
+healthRouter.all("/health", (req, res) => {
+    HttpResponse.ok("API is doing fine.", { message: req.body.message }).send(
+        res,
+    );
+});
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,6 +1,7 @@
 import type { Express } from "express";
 import { weatherRouter } from "./weather_router";
+import { healthRouter } from "./health_router";
 
 export function registerRouters(app: Express) {
-    app.use("/api/v1", weatherRouter);
+    app.use("/api/v1", weatherRouter, healthRouter);
 }
